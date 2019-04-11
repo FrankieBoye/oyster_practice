543cda83c2c1b7d8acf436ff231ea1092bc63ee0
FrankieBoye <frankie4@btinternet.com>
2019-04-11 21:34:26 +0100

    8 - complete

---
+++ lib/oystercard.rb
@@ -0,0 +1,33 @@
+class Oystercard
+
+Maximum_balance = 90
+
+attr_reader :balance, :in_journey
+
+def initialize
+  @balance = 0
+  @in_journey = false
+end
+
+def top_up(amount)
+  fail "Maximum balance of #{Maximum_balance} exceeded" if (@balance + amount) > Maximum_balance
+  @balance += amount
+end
+
+def fare(amount)
+  @balance -= amount
+end
+
+def in_journey?
+ @in_journey
+end
+
+def touch_in
+  @in_journey = true
+end
+
+def touch_out
+  @in_journey = false
+end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,1 +1,37 @@
+require 'oystercard'
+
 describe Oystercard do
+
+it 'must store a balance' do
+  expect(subject.balance).to eq (0)
+end
+
+it 'should start not in journey' do
+  expect(subject).not_to be_in_journey
+end
+
+it 'can touch in' do
+  subject.touch_in
+  expect(subject).to be_in_journey
+end
+
+it 'can touch out' do
+  subject.touch_in
+  subject.touch_out
+  expect(subject).not_to be_in_journey
+end
+
+
+it 'must deduct the fare' do
+  expect{ subject.fare 1 }.to change { subject.balance }.by -1
+end
+
+describe '#top_up' do
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it 'can add money to the balance' do
+    expect{ subject.top_up 1 }.to change { subject.balance }.by 1
+  end
+end
+
+end
